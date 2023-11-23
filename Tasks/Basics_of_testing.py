46c6f26c5fb655eed3cc47b5bbf23c90a2199a16
Fortesa Cena <fortesa.cena@student.uni-pr.edu>
2023-11-23 11:55:00 +0100

    Basics of testing

---
+++ Tasks/Basics_of_testing.py
@@ -0,0 +1,69 @@
+#---------------------------TASK 1-----------------------------
+
+"""Pick your solution to one of the exercises in this module. Design tests for this solution and write 
+tests using unittest library. """
+
+
+import unittest
+
+def add_numbers(a, b):
+    return a + b
+
+class TestAddNumbersFunction(unittest.TestCase):
+    def test_add_positive_numbers(self):
+        result = add_numbers(2, 3)
+        self.assertEqual(result, 5)
+
+    def test_add_negative_numbers(self):
+        result = add_numbers(-2, -3)
+        self.assertEqual(result, -5)
+
+    def test_add_mixed_numbers(self):
+        result = add_numbers(2, -3)
+        self.assertEqual(result, -1)
+
+    def test_add_float_numbers(self):
+        result = add_numbers(2.5, 3.5)
+        self.assertEqual(result, 6.0)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+#---------------------------TASK 2-----------------------------
+"""Write tests for the Phonebook application, which you have implemented in module 1. Design tests 
+for this solution and write tests using unittest library"""
+
+
+import unittest
+
+class TestPhonebookApplication(unittest.TestCase):
+    def setUp(self):
+       
+        self.phonebook = Phonebook()
+        self.phonebook.add_contact("John Doe", "123456789")
+        self.phonebook.add_contact("Jane Doe", "987654321")
+
+    def tearDown(self):
+        
+        self.phonebook.clear()
+
+    def test_add_contact(self):
+        self.phonebook.add_contact("Alice", "111222333")
+        self.assertEqual(self.phonebook.get_contact("Alice"), "111222333")
+
+    def test_get_contact(self):
+        result = self.phonebook.get_contact("John Doe")
+        self.assertEqual(result, "123456789")
+
+    def test_get_nonexistent_contact(self):
+        with self.assertRaises(KeyError):
+            self.phonebook.get_contact("Nonexistent")
+
+    def test_delete_contact(self):
+        self.phonebook.delete_contact("Jane Doe")
+        with self.assertRaises(KeyError):
+            self.phonebook.get_contact("Jane Doe")
+
+if __name__ == '__main__':
+    unittest.main()
