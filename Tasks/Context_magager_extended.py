a01d1024b190f10fabc78b44665c8505d33fa354
Fortesa Cena <fortesa.cena@student.uni-pr.edu>
2023-11-24 16:05:46 +0100

    .

--- Tasks/Context_magager_extended.py
+++ Tasks/Context_magager_extended.py
@@ -34,3 +34,50 @@ mode = "w"
 with File_Context_Manager(filename, mode) as file:
     file.write("Hello, this is a test.")
     
+#---------------------------TASK 2-----------------------------
+
+import unittest
+import os
+
+class TestFileContextManager(unittest.TestCase):
+    def setUp(self):
+        
+        self.filename = "test_file.txt"
+        with open(self.filename, "w") as test_file:
+            test_file.write("Initial content")
+
+    def test_successful_read(self):
+        with File_Context_Manager(self.filename, "r") as file:
+            content = file.read()
+            self.assertEqual(content, "Initial content")
+
+    def test_successful_write(self):
+        with File_Context_Manager(self.filename, "a") as file:
+            bytes_written = file.write("\nAdditional content")
+            self.assertEqual(bytes_written, len("\nAdditional content"))
+
+    def test_nonexistent_file(self):
+        with self.assertRaises(FileNotFoundError):
+            with File_Context_Manager("nonexistent_file.txt", "r") as file:
+                pass
+
+    def test_invalid_mode(self):
+        with self.assertRaises(ValueError):
+            with File_Context_Manager(self.filename, "invalid_mode") as file:
+                pass
+
+    def test_read_after_close(self):
+        with File_Context_Manager(self.filename, "r") as file:
+            pass
+        with self.assertRaises(ValueError):
+            file.read()
+
+    def test_write_after_close(self):
+        with File_Context_Manager(self.filename, "a") as file:
+            pass
+        with self.assertRaises(ValueError):
+            file.write("Attempt to write after close")
+
+if __name__ == '__main__':
+    unittest.main()
+
